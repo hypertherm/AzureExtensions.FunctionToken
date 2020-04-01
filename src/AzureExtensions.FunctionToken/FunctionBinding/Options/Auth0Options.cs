6a3b6fdfa836fedc06ad4ab6c9b58c14cfc76e0d
Harrison Saturley-Hall <h.saturley-hall@hypertherm.com>
2020-04-01 14:47:57 -0400

    Enable Auth0 to use a custom domain
    As Auth0 can handle domain remaps the tenant should be more general

--- src/AzureExtensions.FunctionToken/FunctionBinding/Options/Auth0Options.cs
+++ src/AzureExtensions.FunctionToken/FunctionBinding/Options/Auth0Options.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Threading;
 using AzureExtensions.FunctionToken.FunctionBinding.Options.Interface;
 using Microsoft.IdentityModel.Protocols;
@@ -8,12 +9,15 @@ namespace AzureExtensions.FunctionToken.FunctionBinding.Options
 {
     public sealed class Auth0Options : ITokenOptions
     {
+        public static string Scheme = "https";
+        public static int Port = 443;
+        public static string OpenIdConfigurationPath = "/.well-known/openid-configuration";
         public TokenSigningKeyOptions SigningOptions { get; }
 
-        public Auth0Options(string tenantName, string audience)
+        public Auth0Options(string hostName, string audience)
             : this(
                 new ConfigurationManager<OpenIdConnectConfiguration>(
-                    $"https://{tenantName}.auth0.com/.well-known/openid-configuration",
+                    new UriBuilder(Scheme, hostName, Port, OpenIdConfigurationPath).Uri.AbsoluteUri,
                     new OpenIdConnectConfigurationRetriever()
                 ),
                 audience
